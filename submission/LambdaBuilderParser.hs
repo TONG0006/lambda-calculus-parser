48bfb8c385e0478ea61e8421988e2408cf50c2e7
Timothy Ongko <tong0006@student.monash.edu>
2022-10-18 23:56:57 +1100

    Removed deprecated code implementations

--- submission/LambdaBuilderParser.hs
+++ submission/LambdaBuilderParser.hs
@@ -37,37 +37,3 @@ longLambdaTerms = variables ||| bracket variables ||| bracket longLambda
 
 longLambdaExpression :: Parser Builder
 longLambdaExpression = foldl1 ap <$> list1 longLambdaTerms
-
-
-rawVarExpr :: Parser Builder
-rawVarExpr = foldr1 ap <$> ((term <$>) <$> stringVariables)
-
-rawLamExpr :: Parser Builder
-rawLamExpr = liftA2 lam (is 'λ' *> charVariable <* is '.') multExpr
-
-rawVarLamExpr :: Parser Builder
-rawVarLamExpr = rawVarExpr ||| rawLamExpr
-
-brExpr :: Parser Builder -> Parser Builder
-brExpr =  between (is '(') (is ')')
-
-brLamExpr :: Parser Builder
-brLamExpr = brExpr rawLamExpr
-
-brVarLamExpr :: Parser Builder
-brVarLamExpr = brExpr rawVarLamExpr
-
-expr :: Parser Builder -> Parser Builder
-expr p =  brExpr p ||| p
-
-varLamExpr :: Parser Builder
-varLamExpr = expr rawVarLamExpr
-
-multLamExpr :: Parser Builder
-multLamExpr = foldl1 ap <$> list1 varLamExpr
-
-multBrLamExpr :: Parser Builder
-multBrLamExpr = foldl1 ap <$> list1 brLamExpr
-
-multExpr :: Parser Builder
-multExpr = foldl1 ap <$> list1 varLamExpr
--- submission/LambdaParser.hs
+++ submission/LambdaParser.hs
@@ -64,7 +64,7 @@ longLambdaP = build <$> longLambdaExpression
 -- >>> parse shortLambdaP "(λx.x)(λy.yy)"
 -- Result >< (\x.x)\y.yy
 shortLambdaP :: Parser Lambda
-shortLambdaP = build <$> multLamExpr
+shortLambdaP = undefined
 
 -- | Parses a string representing a lambda calculus expression in short or long form
 -- >>> parse lambdaP "λx.xx"
